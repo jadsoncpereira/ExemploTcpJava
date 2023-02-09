f2c94e9a208a70aaba604e51b39735c555a2ffe1
Ubuntu <ubuntu@ip-172-31-20-212.ec2.internal>
2023-02-09 19:21:41 +0000

    adicionando resposta do servidor

--- src/main/java/br/ufs/dcomp/ExemploTcpJava/TCPClient.java
+++ src/main/java/br/ufs/dcomp/ExemploTcpJava/TCPClient.java
@@ -5,22 +5,34 @@ package br.ufs.dcomp.ExemploTcpJava;
 
 import java.net.*;
 import java.io.*;
-public class TCPClient{
-    public static void main(String[] args){
+public class TCPClient {
+    public static void main(String[] args) {
         try {
             System.out.print("[ Conectando com o Servidor TCP    ..................  ");
             Socket sock = new Socket("127.0.0.1", 3300);
             System.out.println("[OK] ]");
-            
+
             InputStream is = sock.getInputStream(); // Canal de entrada de dados
             OutputStream os = sock.getOutputStream(); // Canal de saída de dados
             String msg = "Olá, DCOMP!!!";
-            byte[] buf = msg.getBytes(); // Obtendo a respresntação em bytes da mensagem
+            byte[] buf = msg.getBytes(); // Obtendo a representação em bytes da mensagem
 
             System.out.print("[ Enviando mensagem    ..............................  ");
             os.write(buf);
             System.out.println("[OK] ]");
-        }catch(Exception e){System.out.println(e);}    
+
+            byte[] buf_resposta = new byte[20]; // buffer de recepção
+            
+            System.out.print("[ Aguardando confimação   ..............  ");
+            is.read(buf_resposta); // Operação bloqueante (aguardando chegada de dados)
+            System.out.println("[OK] ]");
+
+            String resposta = new String(buf_resposta); // Mapeando vetor de bytes recebido para String
+
+            System.out.println("  Confirmação recebida: "+ resposta);
+        } catch(Exception e) {
+            System.out.println(e);
+        }    
         System.out.println("[ FIM ]");
     }
 }
--- src/main/java/br/ufs/dcomp/ExemploTcpJava/TCPServer.java
+++ src/main/java/br/ufs/dcomp/ExemploTcpJava/TCPServer.java
@@ -6,14 +6,14 @@ package br.ufs.dcomp.ExemploTcpJava;
 import java.net.*;
 import java.io.*;
 public class TCPServer{
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+
         try {
             System.out.print("[ Iniciando Servidor TCP    .........................  ");
             ServerSocket ss = new ServerSocket(3300, 5, InetAddress.getByName("127.0.0.1"));
             System.out.println("[OK] ]");
-            
-            System.out.print("[ Aquardando pedidos de conexão    ..................  ");
+
+            System.out.print("[ Aguardando pedidos de conexão    ..................  ");
             Socket sock = ss.accept(); // Operação bloqueante (aguardando pedido de conexão)
             System.out.println("[OK] ]");
             
@@ -24,11 +24,20 @@ public class TCPServer{
             System.out.print("[ Aguardando recebimento de mensagem   ..............  ");
             is.read(buf); // Operação bloqueante (aguardando chegada de dados)
             System.out.println("[OK] ]");
-            
+
             String msg = new String(buf); // Mapeando vetor de bytes recebido para String
-            
+
             System.out.println("  Mensagem recebida: "+ msg);
-        }catch(Exception e){System.out.println(e);}    
+
+            String resposta = "Mensagem transmitida!!!";
+            byte[] buf_resposta = resposta.getBytes(); // Obtendo a representação em bytes da mensagem
+
+            System.out.print("[ Enviando confirmação    ..............................  ");
+            os.write(buf_resposta);
+            System.out.println("[OK] ]");
+        } catch(Exception e) {
+            System.out.println(e);
+        }
         System.out.println("[ FIM ]");
     }
 }
